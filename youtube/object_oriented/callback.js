0f114cd84d6cfc477bb864f19de4c6441efcbed7
thepandeyop <thepandeyop@gmail.com>
2023-07-02 22:41:47 +0530

    adding object_oriented,callback,inheritance file

---
+++ youtube/object_oriented/callback.js
@@ -0,0 +1,43 @@
+// console.log("before making");
+
+// setTimeout(()=>{
+// console.log("after two seconds");
+// },2000);
+
+// console.log("after making");
+
+// const trying = (callback)=>{
+// const result = callback(result);
+// }
+
+
+// trying((result)=>{
+// console.log(result);
+// })
+// trying((result)=>{
+//     trying((resultextra)=>{
+//         trying((resultextraextra)=>{
+//             console.log(result);
+//         })
+//     })
+
+// })
+
+// trying().then((result) => {
+//     console.log(result);
+// })
+// .catch((err) => {
+//   console.log(err);  
+// });
+
+const trying = new Promise((resolve,reject)=>
+{
+const success = " superman"
+if(success){
+    resolve('ok');
+}
+else{
+reject('error');
+}
+}
+)
---
+++ youtube/object_oriented/inheritance.js
@@ -0,0 +1,34 @@
+// // normal class having property and method
+
+// class Person{
+//     /* constructor(name){
+//    this.name = name;
+//     }
+// */
+//    hello(){
+//    return "hello to you" // + this.name;
+//    }
+// }
+
+// const uncle =new Person();
+// uncle.hello();
+
+
+
+// inherited class
+
+// class Humans{
+// breath(){
+//     return 'I can breath';
+// }
+// }
+
+// class Superhuman extends Humans{
+// lazer(){
+//     return 'lazer power unlocked';
+// }
+// }
+
+// const Superman = new Superhuman();
+// console.log(Superman.breath(),Superman.lazer());
+
